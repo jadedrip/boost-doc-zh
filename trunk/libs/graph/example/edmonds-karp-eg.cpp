3f64a59addd084dc0873c6e7a0a7a66e4c3ae669
alai04 <alai04@6ac79471-8550-0410-9800-ef5918a465c8>
2009-06-26 07:01:13 +0000

    graph 库文档第1-7章，由felurkinda翻译，alai04校验。 更新 example 目录

--- trunk/libs/graph/example/edmonds-karp-eg.cpp
+++ trunk/libs/graph/example/edmonds-karp-eg.cpp
@@ -8,12 +8,13 @@
 #include <boost/config.hpp>
 #include <iostream>
 #include <string>
-#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/edmonds_karp_max_flow.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/read_dimacs.hpp>
+#include <boost/graph/graph_utility.hpp>
 
 // Use a DIMACS network flow file as stdin.
-// push-relabel-eg < max_flow.dat
+// edmonds-karp-eg < max_flow.dat
 //
 // Sample output:
 //  c  The total flow:
@@ -21,17 +22,17 @@
 //
 //  c flow values:
 //  f 0 6 3
-//  f 0 1 0
-//  f 0 2 10
+//  f 0 1 6
+//  f 0 2 4
 //  f 1 5 1
 //  f 1 0 0
-//  f 1 3 0
+//  f 1 3 5
 //  f 2 4 4
-//  f 2 3 6
+//  f 2 3 0
 //  f 2 0 0
 //  f 3 7 5
 //  f 3 2 0
-//  f 3 1 1
+//  f 3 1 0
 //  f 4 5 4
 //  f 4 6 0
 //  f 5 4 0
@@ -41,37 +42,41 @@
 //  f 7 6 0
 //  f 7 5 0
 
-
 int
 main()
 {
   using namespace boost;
+
   typedef adjacency_list_traits < vecS, vecS, directedS > Traits;
-  typedef adjacency_list < vecS, vecS, directedS,
+  typedef adjacency_list < listS, vecS, directedS,
     property < vertex_name_t, std::string >,
     property < edge_capacity_t, long,
     property < edge_residual_capacity_t, long,
     property < edge_reverse_t, Traits::edge_descriptor > > > > Graph;
+
   Graph g;
 
   property_map < Graph, edge_capacity_t >::type
     capacity = get(edge_capacity, g);
+  property_map < Graph, edge_reverse_t >::type rev = get(edge_reverse, g);
   property_map < Graph, edge_residual_capacity_t >::type
     residual_capacity = get(edge_residual_capacity, g);
-  property_map < Graph, edge_reverse_t >::type rev = get(edge_reverse, g);
+
   Traits::vertex_descriptor s, t;
   read_dimacs_max_flow(g, capacity, rev, s, t);
 
 #if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
-  property_map<Graph, vertex_index_t>::type indexmap = get(vertex_index, g);
-  long flow = push_relabel_max_flow(g, s, t, capacity, residual_capacity, rev,
-                                    indexmap);
+  std::vector<default_color_type> color(num_vertices(g));
+  std::vector<Traits::edge_descriptor> pred(num_vertices(g));
+  long flow = edmonds_karp_max_flow
+    (g, s, t, capacity, residual_capacity, rev, &color[0], &pred[0]);
 #else
-  long flow = push_relabel_max_flow(g, s, t);
+  long flow = edmonds_karp_max_flow(g, s, t);
 #endif
 
   std::cout << "c  The total flow:" << std::endl;
   std::cout << "s " << flow << std::endl << std::endl;
+
   std::cout << "c flow values:" << std::endl;
   graph_traits < Graph >::vertex_iterator u_iter, u_end;
   graph_traits < Graph >::out_edge_iterator ei, e_end;
@@ -80,5 +85,6 @@ main()
       if (capacity[*ei] > 0)
         std::cout << "f " << *u_iter << " " << target(*ei, g) << " "
           << (capacity[*ei] - residual_capacity[*ei]) << std::endl;
+
   return EXIT_SUCCESS;
 }
